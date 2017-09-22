c4ceed47d626a0eeaf06f17180393a77132de788
Jay Bauer <jbauer@elitedigital.ca>
2017-09-22 11:12:03 -0400

    Tweaks

--- src/helpers/buttons.js
+++ src/helpers/buttons.js
@@ -4,10 +4,10 @@ module.exports = function(type,target,options) {
     markup += 'target="_blank" ';
   }
   if (type == 'buy') {
-    markup += `class="yellow-button" onclick="ga('send', 'event', 'Bed Bath & Beyond', 'Click', 'Buy Online', '1'); console.log('click');"><h4>Buy Online!</h4></a>`;
+    markup += `class="yellow-button" onclick="ga('send', 'event', 'Bed Bath & Beyond', 'Click', 'Buy Online', '1'); goog_report_conversion('${options.fn(this)}'); console.log('click');"><h4>Buy Online!</h4></a>`;
   }
   if (type == 'find') {
-    markup += `class="green-button" onclick="ga('send', 'event', 'Bed Bath & Beyond', 'Click', 'Find a Store', '1'); console.log('click');"><h4>Find a Store!</h4></a>`;
+    markup += `class="green-button" onclick="ga('send', 'event', 'Bed Bath & Beyond', 'Click', 'Find a Store', '1'); goog_report_conversion('${options.fn(this)}'); console.log('click');"><h4>Find a Store!</h4></a>`;
   }
   return markup;
 }
